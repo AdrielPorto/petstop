d1e7380717379b52feb0ff3b02ca3d75ad340cba
Adriel Porto <adriel.porto@dmslog.com>
2023-06-30 14:00:55 -0300

    adjust

--- src/server.js
+++ src/server.js
@@ -1,23 +1,31 @@
-const express = require("express");
-const path = require("path");
-const {
-    index,
-    stop,
-    stops,
-    createStop,
-    saveStop
-} = require("./pages.js");
+// Importar dependencia
+import express from "express";
+import path from "path";
+import { index, stop, stops, createStop, saveStop } from "./pages.js";
 
+// console.log(pages);
+
+// Iniciando o express
+
+const __dirname = path.resolve();
 const server = express();
 server
-    .use(express.urlencoded({ extended: true }))
-    .use(express.static("public"))
-    .set("views", path.join(__dirname, "src/views"))
-    .set("view engine", "hbs")
-    .get("/", index)
-    .get("/stop", stop)
-    .get("/stops", stops)
-    .get("/create-stop", createStop)
-    .post("/save-stop", saveStop);
+  // Utilizando os arquivos estáticos
+  .use(express.urlencoded({ extended: true }))
 
+  // utilizando os arquivos estáticos
+  .use(express.static("public"))
+
+  // Configurar template engine
+  .set("views", path.join(__dirname, "src/views"))
+  .set("view engine", "hbs")
+
+  // rotas da aplicação
+  .get("/", index)
+  .get("/stop", stop)
+  .get("/stops", stops)
+  .get("/create-stop", createStop)
+  .post("/save-stop", saveStop);
+
+// Ligando o servidor
 server.listen(5500);
